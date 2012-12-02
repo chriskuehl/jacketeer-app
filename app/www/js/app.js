6ef40b9715cb342b8bda639fa051191463bd2e02
Chris Kuehl <chris@techxonline.net>
2012-12-01 23:52:57 -0500

    Adding basic loading.

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -12,6 +12,7 @@ function initialize() {
 	initInterface();
 	
 	// load the first screen
+	setScreen(screenIntro);
 }
 
 // create the general interface elements
@@ -32,6 +33,19 @@ function initInterface() {
 		id: "titleBarText"
 	});
 	updateTitle();
+	
+	// create screen container
+	ui.screenContainerParent = $("<div />");
+	ui.screenContainerParent.appendTo(container);
+	ui.screenContainerParent.attr({
+		id: "screenContainerParent"
+	});
+	
+	ui.screenContainer = $("<div />");
+	ui.screenContainer.appendTo(ui.screenContainerParent);
+	ui.screenContainer.attr({
+		id: "screenContainer"
+	});
 }
 
 function updateTitle(newTitle) {
@@ -41,3 +55,78 @@ function updateTitle(newTitle) {
 	
 	ui.titleBarText.text($("title").text());
 }
+
+
+function setScreen(screen) {
+	var targetID = "screen-" + screen.id;
+	
+	// does it already exist?
+	if ($("#" + targetID).length > 0) {
+		$("#" + targetID).remove();
+	}
+	
+	// create a container div for the new screen
+	var container = $("<div />");
+	container.appendTo(ui.screenContainer);
+	container.addClass("screen");
+	container.attr({
+		id: targetID
+	});
+	
+	// general changes
+	updateTitle(screen.title);
+	
+	// render the screen
+	screen.setup(container);
+}
+
+var screenIntro = {
+	id: "intro",
+	title: "Jacketeer 2013",
+	
+	setup: function(container) {
+		container.css({
+			backgroundColor: "rgba(0, 0, 0, 0.3)"
+		});
+		
+		var page = $("<div />");
+		page.appendTo(container);
+		page.css({
+			backgroundColor: "rgba(255, 255, 255, 0.7)",
+			
+			borderRadius: "15px",
+			padding: "80px",
+			
+			position: "absolute",
+			top: "50%",
+			left: "50%",
+			
+			width: "1200px",
+			height: "800px",
+			
+			marginLeft: "-680px",
+			marginTop: "-500px",
+			
+			boxShadow: "0px 20px 40px rgba(0, 0, 0, 0.3)"
+		});
+		
+		var title = $("<p />");
+		title.appendTo(page);
+		title.text("Jacketeer 2013 Student Portal");
+		title.css({
+			textAlign: "center",
+			fontFamily: "\"Helvetica Neue Bold\", \"HelveticaNeue-Bold\"",
+			fontSize: "72px"
+		});
+		
+		var introText = $("<p />");
+		introText.appendTo(page);
+		introText.text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat lorem eget diam accumsan malesuada. Praesent convallis urna tincidunt nunc auctor in interdum est aliquam. Sed euismod turpis non dolor egestas pellentesque. Duis hendrerit nibh metus, a ornare quam. Etiam volutpat lacus in arcu iaculis fermentum. Proin gravida pellentesque velit vel consectetur.");
+		introText.css({
+			fontSize: "28px",
+			marginTop: "30px",
+			lineHeight: "1.8em",
+			textAlign: "justify"
+		});
+	}
+};
