6b30eb77c4d54d79bba6d0f5422f158df8b7f2a0
Chris Kuehl <chris@techxonline.net>
2012-12-07 10:01:30 -0500

    Remove support for closed splines (not needed for handwriting).

--- app/www/js/lib/canvas-spline.js
+++ app/www/js/lib/canvas-spline.js
@@ -29,50 +29,33 @@ function drawSpline(ctx,pts,t,closed){
     var cp=[];   // array of control points, as x0,y0,x1,y1,...
     var n=pts.length;
 
-    if(closed){
-        //   Append and prepend knots and control points to close the curve
-        pts.push(pts[0],pts[1],pts[2],pts[3]);
-        pts.unshift(pts[n-1]);
-        pts.unshift(pts[n-1]);
-        for(var i=0;i<n;i+=2){
-            cp=cp.concat(getControlPoints(pts[i],pts[i+1],pts[i+2],pts[i+3],pts[i+4],pts[i+5],t));
-        }
-        cp=cp.concat(cp[0],cp[1]);   
-        for(var i=2;i<n+2;i+=2){
-            ctx.strokeStyle = "black";    
-            ctx.beginPath();
-            ctx.moveTo(pts[i],pts[i+1]);
-            ctx.bezierCurveTo(cp[2*i-2],cp[2*i-1],cp[2*i],cp[2*i+1],pts[i+2],pts[i+3]);
-            ctx.stroke();
-            ctx.closePath();
-        }
-    }else{  
-        // Draw an open curve, not connected at the ends
-        for(var i=0;i<n-4;i+=2){
-            cp=cp.concat(getControlPoints(pts[i],pts[i+1],pts[i+2],pts[i+3],pts[i+4],pts[i+5],t));
-        }    
-        for(var i=2;i<pts.length-5;i+=2){
-            ctx.strokeStyle= "black";     
-            ctx.beginPath();
-            ctx.moveTo(pts[i],pts[i+1]);
-            ctx.bezierCurveTo(cp[2*i-2],cp[2*i-1],cp[2*i],cp[2*i+1],pts[i+2],pts[i+3]);
-            ctx.stroke();
-            ctx.closePath();
-        }
-        //  For open curves the first and last arcs are simple quadratics.
-        ctx.strokeStyle= "black";
+    // Draw an open curve, not connected at the ends
+    for(var i=0;i<n-4;i+=2){
+        cp=cp.concat(getControlPoints(pts[i],pts[i+1],pts[i+2],pts[i+3],pts[i+4],pts[i+5],t));
+    }    
+    for(var i=2;i<pts.length-5;i+=2){
+        ctx.strokeStyle= "black";     
         ctx.beginPath();
-        ctx.moveTo(pts[0],pts[1]);
-        ctx.quadraticCurveTo(cp[0],cp[1],pts[2],pts[3]);
-        ctx.stroke();
-        ctx.closePath();
-        
-        ctx.strokeStyle="black";
-        ctx.beginPath();
-        ctx.moveTo(pts[n-2],pts[n-1]);
-        ctx.quadraticCurveTo(cp[2*n-10],cp[2*n-9],pts[n-4],pts[n-3]);
+        ctx.moveTo(pts[i],pts[i+1]);
+        ctx.bezierCurveTo(cp[2*i-2],cp[2*i-1],cp[2*i],cp[2*i+1],pts[i+2],pts[i+3]);
         ctx.stroke();
         ctx.closePath();
     }
+    //  For open curves the first and last arcs are simple quadratics.
+    ctx.strokeStyle= "black";
+    ctx.beginPath();
+    ctx.moveTo(pts[0],pts[1]);
+    ctx.quadraticCurveTo(cp[0],cp[1],pts[2],pts[3]);
+    ctx.stroke();
+    ctx.closePath();
+    
+    ctx.strokeStyle="black";
+    ctx.beginPath();
+    ctx.moveTo(pts[n-2],pts[n-1]);
+    ctx.quadraticCurveTo(cp[2*n-10],cp[2*n-9],pts[n-4],pts[n-3]);
+    ctx.stroke();
+    ctx.closePath();
+    
+    
     ctx.restore();
 }
