cbfa6dc601f3ace2bce3c5000f077f9bbca1820d
Chris Kuehl <chris@techxonline.net>
2012-12-07 10:00:26 -0500

    Additional cleanup and fixes on spline library.

--- app/www/js/lib/canvas-spline.js
+++ app/www/js/lib/canvas-spline.js
@@ -1,14 +1,28 @@
-function drawPoint(ctx,x,y,r,color){
-    ctx.save();  
-    ctx.beginPath();
-    //ctx.lineWidth=1;
-    ctx.fillStyle=hexToCanvasColor(color,1);
-    ctx.arc(x,y,r,0.0,2*Math.PI,false);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.fill();
-    ctx.restore();
+function getControlPoints(x0,y0,x1,y1,x2,y2,t){
+    //  x0,y0,x1,y1 are the coordinates of the end (knot) pts of this segment
+    //  x2,y2 is the next knot -- not connected here but needed to calculate p2
+    //  p1 is the control point calculated here, from x1 back toward x0.
+    //  p2 is the next control point, calculated here and returned to become the 
+    //  next segment's p1.
+    //  t is the 'tension' which controls how far the control points spread.
+    
+    //  Scaling factors: distances from this knot to the previous and following knots.
+    var d01=Math.sqrt(Math.pow(x1-x0,2)+Math.pow(y1-y0,2));
+    var d12=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+   
+    var fa=t*d01/(d01+d12);
+    var fb=t-fa;
+  
+    var p1x=x1+fa*(x0-x2);
+    var p1y=y1+fa*(y0-y2);
+
+    var p2x=x1-fb*(x0-x2);
+    var p2y=y1-fb*(y0-y2);  
+    
+    return [p1x,p1y,p2x,p2y]
 }
+
+
 function drawSpline(ctx,pts,t,closed){
     //ctx.lineWidth=4;
     ctx.save();
