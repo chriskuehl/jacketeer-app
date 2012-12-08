1a2570baa1d4a3fce9fee87ae3471547c37fccc5
Chris Kuehl <chris@techxonline.net>
2012-12-08 11:12:22 -0500

    Fixing error in distance formula. Let's not tell any of my math teachers about this one :).

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -408,7 +408,7 @@ function addPenPosition(ctx, canvas, e, ignore) {
 }
 
 function dist(p1, p2) {
-	return ((p1[0] - p1[0]) ^ 2 + (p1[1] - p2[1]) ^ 2) ^ 0.5;
+	return ((p1[0] - p2[0]) ^ 2 + (p1[1] - p2[1]) ^ 2) ^ 0.5;
 }
 
 function redrawCanvas(canvas, ctx) {
