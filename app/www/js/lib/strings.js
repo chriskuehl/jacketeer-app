37a79483576e1d5df492fc86c7d6a9d50d20b3d3
Chris Kuehl <chris@techxonline.net>
2012-12-10 14:29:46 -0500

    Adding credit plus endsWith function to string prototype enhancements.

--- app/www/js/lib/strings.js
+++ app/www/js/lib/strings.js
@@ -1,6 +1,13 @@
+// http://stackoverflow.com/questions/646628/javascript-startswith
 if (typeof String.prototype.startsWith != 'function') {
   // see below for better implementation!
   String.prototype.startsWith = function (str){
     return this.indexOf(str) == 0;
+  };
+}
+
+if (typeof String.prototype.endsWith != 'function') {
+  String.prototype.endsWith = function (str){
+    return this.slice(-str.length) == str;
   };
 }
