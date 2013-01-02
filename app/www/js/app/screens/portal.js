0ce3e072ec441759060ae397fa215b1be02fdb46
Chris Kuehl <chris@techxonline.net>
2013-01-02 11:36:12 -0500

    Adding screenPortal.

---
+++ app/www/js/app/screens/portal.js
@@ -0,0 +1,15 @@
+var screenPortal = {
+	id: "portal",
+	title: "Jacketeer 2013 PORTAL",
+	parent: "into",
+
+	setup: function (container) {
+		container.css({
+			backgroundColor: "rgba(253, 249, 207, 0.2)",
+			boxShadow: "inset 0px 0px 900px rgba(253, 249, 207, 0.8)"
+		});
+
+		var loginDetails = getLoginDetails();
+
+	}
+};
