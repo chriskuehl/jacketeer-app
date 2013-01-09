c04f41b79587016ffaff7a03e82db195dda5df38
Chris Kuehl <chris@techxonline.net>
2013-01-09 18:28:33 -0500

    Finishing hail screen.

--- app/www/js/app/screens/hail-woodford.js
+++ app/www/js/app/screens/hail-woodford.js
@@ -49,21 +49,124 @@ var screenHailWoodford = {
 			boxShadow: "inset 0px 0px 900px rgba(253, 249, 207, 0.8)"
 		});
 
+		var introText2 = $("<h2 />");
+		introText2.appendTo(container);
+		introText2.css({
+			textAlign: "center",
+			fontSize: "96px",
+			lineHeight: "1.6em",
+			margin: "30px",
+			marginTop: "100px",
+			fontFamily: "\"Helvetica Neue Bold\", \"HelveticaNeue-Bold\"",
+			color: "rgba(0, 0, 0, 0.7)"
+		});
+		introText2.html("What do you hail?");
+		
 		var introText = $("<p />");
 		introText.appendTo(container);
 		introText.css({
 			textAlign: "center",
 			fontSize: "38px",
 			lineHeight: "1.6em",
-			margin: "50px",
+			margin: "30px",
+			marginLeft: "100px",
+			marginRight: "100px",
 			color: "rgba(0, 0, 0, 0.7)"
 		});
 		introText.html("Hail Woodford! There's a lot to hail at this school. Help us pick the best things to nominate by filling in the form below. Don't worry&ndash;you can submit as many as you want. If we like your suggestion (or just need to fill space), we'll include it in the book!");
 		
+		// name holder
+		var nameHolder = $("<div />");
+		nameHolder.appendTo(container);
+		nameHolder.css({
+			marginLeft: "auto",
+			marginRight: "auto",
+			width: "1800px"
+		});
+		
+		var nameLabel = $("<p />");
+		nameLabel.css({
+			fontSize: "58px",
+			fontFamily: "\"Helvetica Neue Bold\", \"HelveticaNeue-Bold\"",
+			color: "rgba(0, 0, 0, 0.5)"
+		});
+		nameLabel.appendTo(nameHolder);
+		nameLabel.html("Hail to: ");
+
+		// name
+		var inputName = $("<input type=\"text\" />");
+		inputName.appendTo(nameLabel);
+		inputName.attr({
+			name: "inputName",
+			placeHolder: randomPlaceholderHail(),
+			autoCorrect: "off"
+		});
+		inputName.css({
+			padding: "10px",
+			width: "1150px",
+			marginLeft: "30px",
+			marginRight: "10px",
+			padding: "20px",
+			fontSize: "48px"
+		});
+		inputName.val(name);
+		inputName.keyup(function () {
+			demoName.html($(this).val().replace(/ /g, "<br />"));
+		});
+
+		// submit
+		var submit = $("<input type=\"button\" />");
+		submit.appendTo(nameLabel);
+		submit.css({
+			fontSize: "48px",
+			backgroundColor: "rgba(100, 100, 0, 0.1)",
+			marginLeft: "20px"
+		});
+		submit.val("Submit");
+
+		submit.click(function () {
+			// validate the name they've entered
+			var valid = true;
+			var invalidReason;
+
+			var text = inputName.val().trim();
+
+			if (text.length < 1) {
+				valid = false;
+				invalidReason = "Please enter something to hail.";
+			}
+
+			if (!valid) {
+				return navigator.notification.alert(invalidReason, null, "Invalid Message", "Oops!");
+			}
+
+			// name was valid, so upload it to the server
+			updateInformation({
+				path: "hail.php",
+				data: {
+					user: getLoginDetails().user,
+					token: localStorage.loginToken,
+					text: text
+				}
+			});
+		});
+
+		// submit by enter on input field
+		inputName.keypress(function (e) {
+			if (e.which == 13) {
+				submit.click();
+				$(this).blur(); // hide the iPad keyboard
+
+				e.preventDefault();
+				return false;
+			}
+		});
+
+		
 		var div = $("<div />");
 		div.appendTo(container);
 		div.css({
-			marginTop: "40px",
+			marginTop: "100px",
 			padding: "40px",
 			height: "250px",
 			width: "1800px",
@@ -125,4 +228,18 @@ function exampleHail(hailCounter) {
 			startExampleHailTimer(hailCounter, text.length);
 		});
 	});
+}
+
+function randomPlaceholderHail() {
+	var randomHails = [
+		"all the yerds I used to know",
+		"bonfires of proof pages",
+		"Adobe InDesign and Photoshop",
+		"missing deadlines & missing links",
+		"9.1 TB of available storage space",
+		"the on-the-fly T3i",
+		"an infinite supply of SD cards (hah)"
+	];
+	
+	return randomHails[Math.floor(Math.random() * randomHails.length)];
 }
